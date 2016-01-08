2cc34e216b4f5a2bd26c50115abb3ce6df698488
Curtis L. Olson <curtolson@flightgear.org>
2016-01-08 11:37:41 -0600

    Initial revision.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='props.py',
+      version='1.0',
+      description='Python Property Tree',
+      author='Curtis L. Olson',
+      author_email='curtolson@flightgear.org',
+      url='https://github.com/AuraUAS',
+      py_modules=['props'],
+      #packages=['distutils', 'distutils.command'],
+      #scripts=['props.py']
+     )
