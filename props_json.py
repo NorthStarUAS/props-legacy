1474a4709b4ab61eed336e3fc812cff3a80a6424
Curtis L. Olson <olson126@umn.edu>
2016-09-27 16:16:33 -0500

    Initial test of include + overrides seems to work now (with slightly different syntax from the xml world.)

--- props_json.py
+++ props_json.py
@@ -3,8 +3,6 @@
 import json
 import os.path
 import sys
-#import xml.etree.ElementTree as ET
-import lxml.etree as ET
 import re
 
 from props import PropertyNode, root
@@ -13,9 +11,12 @@ from props import PropertyNode, root
 def parseDict(pynode, newdict, basepath):
     for tag in newdict:
         if type(newdict[tag]) is dict:
-            newnode = PropertyNode()
-            pynode.__dict__[tag] = newnode
-            parseDict(newnode, newdict[tag], basepath)
+            if not tag in pynode.__dict__:
+                node = PropertyNode()
+                pynode.__dict__[tag] = node
+            else:
+                node = pynode.__dict__[tag]
+            parseDict(node, newdict[tag], basepath)
         elif type(newdict[tag]) is list:
             pynode.__dict__[tag] = []
             for ele in newdict[tag]:
@@ -39,116 +40,6 @@ def parseDict(pynode, newdict, basepath):
                 pynode.__dict__[tag] = newdict[tag]
         else:
             print 'skipping:', tag, type(newdict[tag])
-
-# internal dict() tree parsing routine
-def parseDictSimpleInclude(pynode, newdict, basepath):
-    for tag in newdict:
-        if type(newdict[tag]) is dict:
-            newnode = PropertyNode()
-            pynode.__dict__[tag] = newnode
-            parseDictSimpleInclude(newnode, newdict[tag], basepath)
-        elif type(newdict[tag]) is list:
-            pynode.__dict__[tag] = []
-            for ele in newdict[tag]:
-                if type(ele) is dict:
-                    newnode = PropertyNode()
-                    pynode.__dict__[tag].append(newnode)
-                    parseDictSimpleInclude(newnode, ele, basepath)
-                else:
-                    pynode.__dict__[tag].append(ele)
-        elif type(newdict[tag]) is unicode:
-            if re.match('include\s*=\s*', newdict[tag]):
-                # include file
-                file = basepath + re.sub('include\s*=\s*', '', newdict[tag])
-                print 'include:', file
-                newnode = PropertyNode()
-                pynode.__dict__[tag] = newnode
-                load(file, newnode)
-            else:
-                # normal case
-                pynode.__dict__[tag] = newdict[tag]
-        else:
-            print 'skipping:', tag, type(newdict[tag])
-
-# old xml parsing       
-def parseXML(pynode, xmlnode, basepath):
-    overlay  = 'overlay' in xmlnode.attrib
-    exists = xmlnode.tag in pynode.__dict__
-    if len(xmlnode) or 'include' in xmlnode.attrib:
-        # has children
-        newnode = PropertyNode()
-        if 'include' in xmlnode.attrib:
-            filename = basepath + '/' + xmlnode.attrib['include']
-            print "calling load():", filename, xmlnode.attrib
-            load(filename, newnode)
-        if 'n' in xmlnode.attrib:
-            # enumerated node
-            n = int(xmlnode.attrib['n'])
-            if not exists:
-                pynode.__dict__[xmlnode.tag] = []
-            elif not type(pynode.__dict__[xmlnode.tag]) is list:
-                savenode = pynode.__dict__[xmlnode.tag]
-                pynode.__dict__[xmlnode.tag] = [ savenode ]
-            tmp = pynode.__dict__[xmlnode.tag]
-            pynode.extendEnumeratedNode(tmp, n)
-            pynode.__dict__[xmlnode.tag][n] = newnode
-        elif exists:
-            if not overlay:
-                # append
-                # print "node exists:", xmlnode.tag, "overlay:", overlay
-                if not type(pynode.__dict__[xmlnode.tag]) is list:
-                    # we need to convert this to an enumerated list
-                    print "converting node to enumerated:", xmlnode.tag
-                    savenode = pynode.__dict__[xmlnode.tag]
-                    pynode.__dict__[xmlnode.tag] = [ savenode ]
-                pynode.__dict__[xmlnode.tag].append(newnode)
-            else:
-                # overlay (follow existing tree)
-                newnode = pynode.__dict__[xmlnode.tag]
-        else:
-            # create new node
-            pynode.__dict__[xmlnode.tag] = newnode
-        for child in xmlnode:
-            _parseXML(newnode, child, basepath)
-    else:
-        # leaf
-        value = xmlnode.text
-        if 'type' in xmlnode.attrib:
-            if xmlnode.attrib['type'] == 'bool':
-                print xmlnode.tag, "is bool"
-                if value == '0' or value == 'false' or value == '':
-                    value = False
-                else:
-                    value = True
-        if 'n' in xmlnode.attrib:
-            # enumerated node
-            n = int(xmlnode.attrib['n'])
-            if not exists:
-                pynode.__dict__[xmlnode.tag] = []
-            elif not type(pynode.__dict__[xmlnode.tag]) is list:
-                savenode = pynode.__dict__[xmlnode.tag]
-                pynode.__dict__[xmlnode.tag] = [ savenode ]
-            tmp = pynode.__dict__[xmlnode.tag]
-            pynode.extendEnumeratedLeaf(tmp, n, "")
-            pynode.__dict__[xmlnode.tag][n] = value
-            # print "leaf:", xmlnode.tag, value, xmlnode.attrib
-        elif exists:
-            if not overlay:
-                # append
-                if not type(pynode.__dict__[xmlnode.tag]) is list:
-                    # convert to enumerated.
-                    print "converting node to enumerated"
-                    savenode = pynode.__dict__[xmlnode.tag]
-                    pynode.__dict__[xmlnode.tag] = [ savenode ]
-                pynode.__dict__[xmlnode.tag].append(value)
-            else:
-                # overwrite
-                pynode.__dict__[xmlnode.tag] = value
-        elif type(xmlnode.tag) is str:
-            pynode.__dict__[xmlnode.tag] = value
-        else:
-            # print "Skipping unknown node:", xmlnode.tag, ":", value
-            pass
                 
 # load a json file and create a property tree rooted at the given node
 # supports "mytag": "include=relative_file_path.json"
