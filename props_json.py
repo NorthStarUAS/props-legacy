84f972139fc5a3f7ab74f2696c75e9cc70e209ef
Curtis L. Olson <olson126@umn.edu>
2016-09-28 10:37:59 -0500

    Write a small function to 'autodetect' data types based on content. Trim debug output.

--- props_json.py
+++ props_json.py
@@ -7,6 +7,28 @@ import re
 
 from props import PropertyNode, root
 
+def mydecode(value):
+    #print 'mydecode:', value
+    # test for int
+    result = re.match('[-+]?\d+', value)
+    if result:
+        if result.group(0) == value:
+            #print 'int:', value
+            return int(value)
+    # test for float
+    result = re.match('[-+]?\d*\.\d+', value)
+    if result:
+        if result.group(0) == value:
+            #print 'float:', value
+            return float(value)
+    # test for bool
+    if value == 'True' or value == 'true':
+        return True
+    elif value == 'False' or value == 'false':
+        return False
+    # otherwise return the value as a string
+    return str(value)
+     
 # internal dict() tree parsing routine
 def parseDict(pynode, newdict, basepath):
     if 'include' in newdict:
@@ -34,17 +56,18 @@ def parseDict(pynode, newdict, basepath):
                     pynode.__dict__[tag].append(newnode)
                     parseDict(newnode, ele, basepath)
                 else:
-                    pynode.__dict__[tag].append(ele)
+                    pynode.__dict__[tag].append(mydecode(ele))
         elif type(newdict[tag]) is unicode:
             if tag == 'include':
                 # already handled
                 pass
             else:
                 # normal case
-                print 'normal case:', tag, newdict[tag]
-                pynode.__dict__[tag] = newdict[tag]
+                #print 'normal case:', tag, newdict[tag]
+                mydecode(newdict[tag])
+                pynode.__dict__[tag] = mydecode(newdict[tag])
         else:
-            print 'skipping:', tag, type(newdict[tag])
+            print 'json parse skipping:', tag, type(newdict[tag])
                 
 # load a json file and create a property tree rooted at the given node
 # supports "mytag": "include=relative_file_path.json"
@@ -55,8 +78,8 @@ def load(filename, pynode):
         file = f.read()
         # support // style comments in json files (by removing them
         # before passing the file to the json parser)
-        file = re.sub('\n\s*//.*\n', '\n', file)
-        print file
+        file = re.sub('\s*//.*\n', '\n', file)
+        # print file
         newdict = json.loads(file)
         f.close()
     except:
@@ -69,7 +92,7 @@ def load(filename, pynode):
 
 def buildDict(root, pynode):
     for child in pynode.__dict__:
-        print child
+        # print child
         node = pynode.__dict__[child]
         if isinstance(node, PropertyNode):
             root[child] = dict()
@@ -84,10 +107,10 @@ def buildDict(root, pynode):
                 else:
                     root[child].append(str(ele))
    
-        elif type(child) is str:
+        elif type(child) is str or type(child) is unicode:
             root[child] = str(node)
         else:
-            print "skipping:", child, ":", str(node)
+            print "json build skipping:", child, ":", str(node), type(child)
         
 # save the property tree starting at pynode into a json xml file.
 def save(filename, pynode=root):
@@ -95,7 +118,7 @@ def save(filename, pynode=root):
     buildDict(root, pynode)
     try:
         f = open(filename, 'w')
-        json.dump(root, f, indent=2, sort_keys=True)
+        json.dump(root, f, indent=4, sort_keys=True)
         f.close()
     except:
         print filename + ": json save error:\n" + str(sys.exc_info()[1])
